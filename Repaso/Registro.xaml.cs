33dd4c27cf1e162964e0f54232b9098be173634d
master <master@SMLAB408PC15_23>
2023-01-03 19:06:36 -0500

    Agregar archivos de proyecto.

---
+++ Repaso/Registro.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Repaso.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usuario)
+        {
+            InitializeComponent();
+
+            lblUsuario.Text = "Bienvenido" + usuario;
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+
+            try
+            {
+                double precio = Convert.ToDouble(txtPrecio.Text);
+                int cantidadx = Convert.ToInt32(txtCantidad.Text);
+
+                double valor = precio + cantidadx;
+                DisplayAlert("VALOR TOTAL DEL PRODUCTO", valor.ToString(), "Cerrar");
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
+
+
+
+            double precio = Convert.ToDouble(txtPrecio.Text);
+            int cantidad = Convert.ToInt32((txtCantidad.Text));
+
+            double valor = precio + cantidad;
+            DisplayAlert("VALOR TOTAL DEL PRODUCTO", valor.ToString(), "Cerrar");
+            
+        }
+
+    }
+}
---
+++ Repaso/Vistas/Inicio.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Repaso.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        public void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registro(txtUsuario.Text));
+        }
+
+        public void btnCancelar_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
